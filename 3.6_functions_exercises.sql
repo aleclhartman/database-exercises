0e193b91a7150e725fefa7a4a1ed9ebe232219ae
Alec Hartman <aleclhartman08@gmail.com>
2020-02-11 11:26:03 -0600

    ordered some stuff, tried different approach on othe stuff

--- 3.6_functions_exercises.sql
+++ 3.6_functions_exercises.sql
@@ -139,14 +139,15 @@ SELECT *,
 	CONCAT(first_name, ' ', last_name) AS full_name
 FROM employees
 WHERE last_name LIKE 'e%'
-	AND last_name LIKE '%e';
+	AND last_name LIKE '%e'
+ORDER BY emp_no DESC;
 
 /* Convert the names produced in your last query to all uppercase. */
 SELECT *,
-	UPPER(CONCAT(first_name, ' ', last_name)) AS full_name
+	UPPER(CONCAT(first_name, ' ', last_name)) AS upper_full_name
 FROM employees
-WHERE last_name LIKE 'e%'
-	AND last_name LIKE '%e';
+WHERE last_name LIKE 'e%e'
+ORDER BY emp_no DESC;
 	
 /* For your query of employees born on Christmas and hired in the 90s, use datediff() to find how many days they have been working at the company */
 SELECT
@@ -155,7 +156,8 @@ SELECT
 FROM employees
 WHERE YEAR(hire_date) BETWEEN 1990 AND 1999
 	AND MONTH(birth_date) = 12
-	AND DAY(birth_date) = 25;
+	AND DAY(birth_date) = 25
+ORDER BY birth_date, hire_date DESC;
 
 
 /* Find the smallest and largest salary from the salaries table */
@@ -166,7 +168,28 @@ FROM salaries;
 
 # Create employee username
 SELECT
-	CONCAT(LOWER(LEFT(first_name, 1)), LOWER(LEFT(last_name, 4)), "_", SUBSTR(birth_date, 6, 2), SUBSTR(birth_date, 3, 2)) AS username,	
+	CONCAT(
+		LOWER(LEFT(first_name, 1)),
+		LOWER(LEFT(last_name, 4)),
+		"_",
+		SUBSTR(birth_date, 6, 2),
+		SUBSTR(birth_date, 3, 2)
+			) AS username,	
+	first_name,
+	last_name,
+	birth_date
+FROM employees
+LIMIT 10;
+
+# Create employee username - a different approach for month
+SELECT
+	CONCAT(
+		LOWER(LEFT(first_name, 1)),
+		LOWER(LEFT(last_name, 4)),
+		"_",
+		LPAD(MONTH(birth_date), 2, 0),
+		SUBSTR(birth_date, 3, 2)
+			) AS username,	
 	first_name,
 	last_name,
 	birth_date
