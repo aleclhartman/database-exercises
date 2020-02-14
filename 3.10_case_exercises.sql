5d8ca30a36f3e138d9d0a0f64a255fb1fc874f15
Alec Hartman <aleclhartman08@gmail.com>
2020-02-14 14:53:45 -0600

    renamed case exercises and solved more exercises

--- 3.10_case_exercises.sql
+++ 3.10_case_exercises.sql
@@ -1,13 +1,9 @@
 USE employees;
 
 /* Write a query that returns all employees (emp_no), their department number, their start date, their end date, and a new column 'is_current_employee' that is a 1 if the employee is still with the company and 0 if not. */
-SELECT e.emp_no, de.dept_no, e.hire_date, IF(s.to_date != '9999-01-01', MAX(s.to_date), s.to_date),
-	IF(de.to_date = '9999-01-01' AND s.to_date = '9999-01-01', 1, 0) AS is_current_employee
+SELECT e.emp_no,  e.hire_date
 FROM employees AS e
-JOIN dept_emp AS de ON e.emp_no = de.emp_no
-JOIN salaries AS s ON e.emp_no = s.emp_no
-ORDER BY e.emp_no
-GROUP BY e.emp_no;
+JOIN dept_emp AS de ON e.emp_no = de.emp_no;
 
 /* Write a query that returns all employee names, and a new column 'alpha_group' that returns 'A-H', 'I-Q', or 'R-Z' depending on the first letter of their last name. */
 SELECT CONCAT(first_name, ' ', last_name) AS full_name, 
@@ -35,3 +31,20 @@ SELECT
 		) AS employees_born
 FROM employees
 GROUP BY decade;
+
+
+/* What is the average salary for each of the following department groups: R&D, Sales & Marketing, Prod & QM, Finance & HR, Customer Service? */
+SELECT
+	CASE
+		WHEN d.dept_name IN ('Finance', 'Human Resources') THEN 'Finance & HR'
+		WHEN d.dept_name IN ('Sales', 'Marketing') THEN 'Sales & Marketing'
+		WHEN d.dept_name IN ('Production', 'Quality Management') THEN 'Prod & QM'
+		WHEN d.dept_name IN ('Research', 'Development') THEN 'R&D'
+		ELSE d.dept_name
+	END AS dept_group,
+	AVG(s.salary) AS avg_salary
+FROM departments AS d
+JOIN dept_emp AS de ON d.dept_no = de.dept_no
+JOIN salaries AS s ON de.emp_no = s.emp_no
+WHERE de.to_date > CURDATE() AND s.to_date > CURDATE()
+GROUP BY dept_group;
