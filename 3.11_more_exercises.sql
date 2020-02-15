960ca5c5216aa75d89b6618af7683b921a53c816
Alec Hartman <aleclhartman08@gmail.com>
2020-02-14 19:39:52 -0600

    completed exercise one

--- 3.11_more_exercises.sql
+++ 3.11_more_exercises.sql
@@ -1,11 +1,24 @@
 USE employees;
 
-SELECT d.dept_name, SELECT( CONCAT(m.first_name, ' ', m.last_name) AS manager, ms.salary AS manager_salary, AVG(ds.salary) AS avg_salary
-FROM dept_manager AS dm
-LEFT JOIN employees AS m ON dm.emp_no = m.emp_no
+/* How much do the current managers of each department get paid, relative to the average salary for the department? Is there any department where the department manager gets paid less than the average salary? */
+SELECT e.emp_no, d.dept_name, CONCAT(e.first_name, ' ', e.last_name) AS manager_name, ms.salary, AVG(es.salary) AS average_salary
+FROM employees AS e
+JOIN dept_manager AS dm ON e.emp_no = dm.emp_no
 JOIN departments AS d ON d.dept_no = dm.dept_no
 JOIN dept_emp AS de ON d.dept_no = de.dept_no
-JOIN salaries AS ms ON m.emp_no = ms.emp_no
-JOIN salaries as ds ON ds.emp_no = de.emp_no
-WHERE dm.to_date > CURDATE() AND ms.to_date > CURDATE() AND ds.to_date > CURDATE() AND de.to_date > CURDATE()
-GROUP BY d.dept_name;
+JOIN salaries AS ms ON ms.emp_no = dm.emp_no
+JOIN salaries AS es ON es.emp_no = de.emp_no
+WHERE dm.to_date > CURDATE() AND ms.to_date > CURDATE() AND de.to_date > CURDATE() AND es.to_date > CURDATE()
+GROUP BY e.emp_no, d.dept_name, manager_name, ms.salary
+ORDER BY d.dept_name;
+
+
+
+
+
+
+
+
+
+
+
