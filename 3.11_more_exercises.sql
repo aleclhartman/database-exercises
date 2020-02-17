e61cfe7051f38e80e4ba83a506798c03ad4d714c
Alec Hartman <aleclhartman08@gmail.com>
2020-02-17 17:40:36 -0600

    completed exercises through 7b using LIMIT

--- 3.11_more_exercises.sql
+++ 3.11_more_exercises.sql
@@ -246,6 +246,147 @@ GROUP BY c.name
 ORDER BY gross_revenue DESC
 LIMIT 5;
 
+/* SELECT statements
+	 Select all columns from the actor table*/
+SELECT *
+FROM actor;
+
+/* Select only the last_name column from the actor table. */
+SELECT last_name
+FROM actor;
+
+/* Select only the following columns from the film table. */
+SELECT *
+FROM film;
+
+/* DISTINCT operator
+	 Select all distinct (different) last names from the actor table.*/
+SELECT DISTINCT(last_name)
+FROM actor;
+
+/* Select all distinct (different) postal codes from the address table */
+SELECT DISTINCT(postal_code)
+FROM address;
+
+/* Select all distinct (different) ratings from the film table */
+SELECT DISTINCT(rating)
+FROM film;
+
+/* WHERE clause
+	 Select the title, description, rating, movie length columns from the films table that last 3 hours or longer.*/
+SELECT title, description, rating, length
+FROM film
+WHERE length >= 180;
+
+/* Select the payment id, amount, and payment date columns from the payments table for payments made on or after 05/27/2005. */
+SELECT payment_id, amount, payment_date
+FROM payment
+WHERE payment_date >= '2005-05-27'
+ORDER BY payment_date;
+
+/* Select the primary key, amount, and payment date columns from the payment table for payments made on 05/27/2005. */
+DESCRIBE payment;
+
+SELECT payment_id, amount, payment_date
+FROM payment
+WHERE payment_date LIKE '2005-05-27%';
+
+/* Select all columns from the customer table for rows that have a last names beginning with S and a first names ending with N. */
+SELECT *
+FROM customer
+WHERE LEFT(last_name, 1) IN ('S')
+AND RIGHT(first_name, 1) IN ('N');
+
+/* Select all columns from the customer table for rows where the customer is inactive or has a last name beginning with "M". */
+SELECT *
+FROM customer
+WHERE active = False
+OR LEFT(last_name, 1) = 'M';
+
+/* Select all columns from the category table for rows where the primary key is greater than 4 and the name field begins with either C, S or T. */
+SELECT *
+FROM category
+WHERE category_id > 4
+AND LEFT(name, 1) IN ('C', 'S', 'T');
+
+/* Select all columns minus the password column from the staff table for rows that contain a password. */
+SELECT staff_id, first_name, last_name, address_id, picture, email, store_id, active, username, last_update
+FROM staff
+WHERE password IS NOT NULL;
+
+/* Select all columns minus the password column from the staff table for rows that do not contain a password. */
+SELECT staff_id, first_name, last_name, address_id, picture, email, store_id, active, username, last_update
+FROM staff
+WHERE password IS NULL;
+
+/* IN operator
+	Select the phone and district columns from the address table for addresses in California, England, Taipei, or West Java. */
+SELECT phone, district
+FROM address
+WHERE district IN ('California', 'England', 'Taipei', 'West Java');
+
+/* Select the payment id, amount, and payment date columns from the payment table for payments made on 05/25/2005, 05/27/2005, and 05/29/2005. */
+SELECT payment_id, amount, DATE(payment_date)
+FROM payment
+WHEN DATE(payment_date) IN ('2005-05-25', '2005-05-27', '2005-05-29');
+
+/* Select all columns from the film table for films rated G, PG-13 or NC-17 */
+SELECT *
+FROM film
+WHERE rating IN ('G', 'PG-13', 'NC-17');
+
+/* BETWEEN operator
+	Select all columns from the payment table for payments made between midnight 05/25/2005 and 1 second before midnight 05/26/2005. */
+SELECT *
+FROM payment
+WHERE payment_date BETWEEN '2005-05-25 00:00:00' AND  '2005-05-25 23:59:59';
+
+/* Select the following columns from the film table for films where the length of the description is between 100 and 120. */
+SELECT *
+FROM film
+WHERE LEN(description) BETWEEN 100 AND 120;
+
+/* LIKE operator 
+	Select the following columns from the film table for rows where the description begins with "A Thoughtful".*/
+SELECT *
+FROM film
+WHERE description LIKE 'A thoughtful%';
+
+/* Select the following columns from the film table for rows where the description ends with the word "Boat". */
+SELECT *
+FROM film
+WHERE description LIKE '%boat';
+
+/* Select the following columns from the film table where the description contains the word "Database" and the length of the film is greater than 3 hours. */
+SELECT *
+FROM film
+WHERE description LIKE '%datebase%'
+AND length > 180;
+
+/* LIMIT Operator
+	Select all columns from the payment table and only include the first 20 rows. */
+SELECT *
+FROM payment
+LIMIT 20;
+
+/* Select the payment date and amount columns from the payment table for rows where the payment amount is greater than 5, and only select rows whose zero-based index in the result set is between 1000-2000. */
+SELECT payment_date, amount
+FROM payment
+WHERE amount > 5
+LIMIT 1000 OFFSET 999;
+
+/* Select all columns from the customer table, limiting results to those where the zero-based index is between 101-200.
+
+
+
+
+
+
+
+
+
+
+
 
 
 
